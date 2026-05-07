7cb29275bc1cb3f1de673dcc0195f042d2aeb548
bryanping <2570886367@qq.com>
2026-05-07 10:11:40 +0800

    Initial Commit

---
+++ LifeSubtraction/LifeSubtractionApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeSubtractionApp.swift
+//  LifeSubtraction
+//
+//  Created by 林平 on 2026/5/7.
+//
+
+import SwiftUI
+
+@main
+struct LifeSubtractionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
